b453d9496792c2cb5e540373ddd18632478eeae6
Sam Bokai <7689616+sambokai@users.noreply.github.com>
2017-12-07 18:23:01 +0100

    Add Webpage parsing class
    Webpage class can parse page title and html version

---
+++ Learning Projects/S24_TechChallenge/app/models/Webpage.scala
@@ -0,0 +1,35 @@
+package models
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, DocumentType}
+
+import scala.collection.JavaConverters._
+
+class Webpage(url: String) {
+  val doc: Document = Jsoup.connect(url).get()
+
+  val title: String = doc.title()
+
+  val html_version: HTMLVersion = {
+    val doctypeInDOM = {
+      doc
+        .childNodes()
+        .asScala
+        .find(documentType => documentType.isInstanceOf[DocumentType])
+        .map(node => node.asInstanceOf[DocumentType])
+    }
+
+    doctypeInDOM match {
+      case Some(doctype) => doctype.attributes.get("publicId") match {
+        case "" => HTMLVersion.HTML5
+        case doctype_string if doctype_string contains "4.01" => HTMLVersion.HTML4_01
+        case doctype_string if doctype_string contains "XHTML 1.0" => HTMLVersion.XHTML1_0
+        case doctype_string if doctype_string contains "DTD XHTML 1.1" => HTMLVersion.XHTMLDTD1_1
+        case doctype_string if doctype_string contains "XHTML Basic 1.1" => HTMLVersion.XHTMLBasic1_1
+        case _ => HTMLVersion.Unknown
+      }
+      case None => HTMLVersion.Unknown
+    }
+  }
+
+}
---
+++ Learning Projects/S24_TechChallenge/test/models/WebpageTest.scala
@@ -0,0 +1,43 @@
+package models
+
+import org.scalatestplus.play.PlaySpec
+
+class WebpageTest extends PlaySpec {
+
+  "A Webpage" must {
+
+    // TODO: implement non-networking tests. Meaning that the webpages-to-be-tested should exist as html copy inside the project directory and not be pulled from the live website.
+
+    "Retrieve a website document via URL" in {
+      val testurl = "http://example.com/"
+      new Webpage(testurl)
+    }
+
+    "retrieve the page title of a webpage" in {
+      val testurl = "http://example.com/"
+      val testpage = new Webpage(testurl)
+
+      testpage.title mustBe "Example Domain"
+    }
+
+
+    "detect html version 5 in a webpage" in {
+      val testurl = "http://example.com/"
+      val testpage = new Webpage(testurl)
+      testpage.html_version mustBe HTMLVersion.HTML5
+    }
+
+    "detect html version 4.1 in a webpage" in {
+      val testurl = "https://www.w3.org/TR/1999/REC-html401-19991224/"
+      val testpage = new Webpage(testurl)
+      testpage.html_version mustBe HTMLVersion.HTML4_01
+    }
+
+    "detect unknown html version / missing version-string in a webpage" in {
+      val testurl = "http://www.lost-world.com:80/ingen/index.html"
+      val testpage = new Webpage(testurl)
+      testpage.html_version mustBe HTMLVersion.Unknown
+    }
+
+  }
+}
