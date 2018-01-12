8c6cb1f65996e3e055fe99602f586839a42d0cb1
Sam Bokai <7689616+sambokai@users.noreply.github.com>
2018-01-12 15:58:14 +0100

    Remove HtmlAnalyzerController code and routing

--- Learning Projects/S24_TechChallenge/app/controllers/HtmlAnalyzerController.scala
+++ Learning Projects/S24_TechChallenge/app/controllers/HtmlAnalyzerController.scala
@@ -2,33 +2,10 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import controllers.WebsiteForm._
-import play.api.data.Form
 import play.api.mvc._
 import services.HtmlAnalyzer
-import utils.UrlWrapper
 
 @Singleton
 class HtmlAnalyzerController @Inject()(htmlAnalyzer: HtmlAnalyzer, messagesAction: MessagesActionBuilder, components: ControllerComponents) extends AbstractController(components) {
-  private val postUrl = routes.HtmlAnalyzerController.post()
 
-  // TODO: save list of analyzed websites, in order to show the history of analyzed sites as a list on the frontend
-
-  def index = messagesAction { implicit request: MessagesRequest[AnyContent] =>
-    Ok(views.html.HtmlAnalyzer(None, form, postUrl))
-  }
-
-  def post = messagesAction { implicit request: MessagesRequest[AnyContent] =>
-
-    def errorFunction = { formWithErrors: Form[Data] =>
-      BadRequest(views.html.HtmlAnalyzer(None, formWithErrors, postUrl))
-    }
-
-    def successFunction = { data: Data =>
-      Ok(views.html.HtmlAnalyzer(Some(htmlAnalyzer.analyze(new UrlWrapper(data.url))), form, postUrl))
-    }
-
-    def formValidationResult = form.bindFromRequest
-    formValidationResult.fold(errorFunction, successFunction)
-  }
 }
