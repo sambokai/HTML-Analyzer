1e9b0e84db74dc1bb2d0606be533af8c69742f9b
Sam Bokai <7689616+sambokai@users.noreply.github.com>
2018-02-08 13:01:33 +0100

    Fix: add missing space in catchall error message

--- app/controllers/HtmlAnalyzerController.scala
+++ app/controllers/HtmlAnalyzerController.scala
@@ -18,14 +18,14 @@ class HtmlAnalyzerController @Inject()(htmlAnalyzer: HtmlAnalyzer, messagesActio
   }
 
 
-  def triggerAnalysis = Action.async { implicit request =>
+  def triggerAnalysis: Action[AnyContent] = Action.async { implicit request =>
     val formData = WebsiteForm.urlAnalysisForm.bindFromRequest
 
     htmlAnalyzer.analyze(formData.data("url")).map {
       case Right(webpage: WebPage) => Ok(views.html.HtmlAnalyzerView(Some(webpage), WebsiteForm.urlAnalysisForm, postUrl, None))
       case Left(errorMessage: String) => BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some(errorMessage)))
     }.recover {
-      case e => BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some("Something went wrong. Please try again or contact us." + e)))
+      case e => BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some("Something went wrong. Please try again or contact us. " + e)))
     }
   }
 }
