62171c8518bf1e367a2bc46664e3b9a3449b65f5
Sam Bokai <7689616+sambokai@users.noreply.github.com>
2018-02-09 15:17:59 +0100

    Log unexcpected exceptions of WebPage analysis in controller

--- app/controllers/HtmlAnalyzerController.scala
+++ app/controllers/HtmlAnalyzerController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import domain.WebPage
+import play.api.Logger
 import play.api.mvc._
 import services.HtmlAnalyzer
 
@@ -13,6 +14,8 @@ import scala.util.matching.Regex
 class HtmlAnalyzerController @Inject()(htmlAnalyzer: HtmlAnalyzer, messagesAction: MessagesActionBuilder, components: ControllerComponents) extends AbstractController(components) with play.api.i18n.I18nSupport {
 
   import HtmlAnalyzerController._
+
+  val logger: Logger = Logger(this.getClass)
 
   private val postUrl = routes.HtmlAnalyzerController.triggerAnalysis()
 
@@ -34,7 +37,9 @@ class HtmlAnalyzerController @Inject()(htmlAnalyzer: HtmlAnalyzer, messagesActio
       case Right(webpage: WebPage) => Ok(views.html.HtmlAnalyzerView(Some(webpage), WebsiteForm.urlAnalysisForm, postUrl, None))
       case Left(errorMessage: String) => BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some(errorMessage)))
     }.recover {
-      case e => BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some("Something went wrong. Please try again or contact us. " + e)))
+      case e =>
+        logger.error(s"Unexpected error", e)
+        BadRequest(views.html.HtmlAnalyzerView(None, WebsiteForm.urlAnalysisForm, postUrl, Some("Something went wrong. Please try again or contact us. " + e)))
     }
   }
 }
